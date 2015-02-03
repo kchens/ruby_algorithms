5188e1c283062a1a8ee657efbe66a605c8cdef8a
Kevin K Chen <kevin.kang.chen@gmail.com>
2015-02-03 13:08:12 -0800

    binary_search_tree skeleton

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,8 +1,35 @@
 class Node
 
+  attr_reader :value
+  attr_accessor :left, :right
+
   def initialize(v)
+    @value = v
+    @left = nil
+    @right = nil
+  end
+
+  def push(v)
+    case value <=> v
+    when -1 then left = Node.new(v)
+  end
+
+  def include?(v)
 
   end
 
+  def inspect
+    "#{value}:  Left - #{left.inspect}; Right - #{right.inspect}"
+  end
 
+
+  private
+
+  def push_left(v)
+
+  end
+
+  def push_right(v)
+
+  end
 end
